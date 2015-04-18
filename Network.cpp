6ce3ea4f3ff8a993b271772bdc158716659368f6
Marcelino Coll <coll.marce@gmail.com>
2015-04-18 19:03:34 +0200

    Added network and City classes (this one still a placeholder)

---
+++ Network.cpp
@@ -0,0 +1,52 @@
+#include "Network.h"
+
+#include<vector>
+
+Network::Network() {
+    init();
+}
+
+void Network::init() {
+    for (int i = 0; i < num_cities; i++) {
+        cities[i] = City(i);
+        for (int j = 0; j < num_cities; j++) {
+            adjacency_table[i][j] = false;
+        }
+    }
+}
+
+Network::~Network() {}
+
+void Network::addPath(int a, int b) {
+    changePath(a, b, true);
+}
+
+void Network::destroyNode(int n) {
+    for (int i = 0; i < num_cities; i++) {
+        changePath(n, i, false);
+    }
+}
+
+void Network::changePath(int a, int b, bool value) {
+    adjacency_table[a][b] = b; 
+    adjacency_table[b][a] = b;
+}
+
+// Crec que hauria de funcionar, no n'estic gens segur per aixo haha
+bool Network::isValidPath(std::vector<int> path) {
+    bool adj_table[num_cities][num_cities];
+    memcpy(adj_table, adjacency_table, sizeof(adjacency_table));
+
+    int previous = path[0];
+    std::vector<int>::iterator it = path.begin; it++;
+    for (; it != path.end; it++) {
+        if (adj_table[*it][previous] == true) {
+            changePath(*it, previous, false);
+            previous = *it;
+        }
+        else {
+            return false;
+        }
+    }
+    return true;
+}
---
+++ Network.h
@@ -0,0 +1,27 @@
+#ifndef __LD32_NETWORK_H__
+#define __LD32_NETWORK_H__
+
+#include "City.h"
+
+const int num_cities = 5;
+
+class Network
+{
+public:
+    Network();
+    ~Network();
+
+    void init();
+
+    void addPath(int a, int b);
+    void destroyNode(int n);
+
+    bool isValidPath(std::vector<int> path);
+private:
+    City cities[num_cities];
+    bool adjacency_table[num_cities][num_cities];
+
+    void changePath(int a, int b, bool value);
+};
+
+#endif
