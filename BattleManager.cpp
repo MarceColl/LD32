bc9afcff6d48c2eb8b23a92488539703f5fc0e91
Jordi <jordi@weathercloud.net>
2015-04-18 23:48:35 +0200

    Half Implemented Battle Manager

---
+++ BattleManager.cpp
@@ -0,0 +1,5 @@
+#include "BattleManager.h"
+
+void BattleManager::resolveBattle(Beasts *beasts1, Beasts *beasts2) {
+    beasts2->beasts.clear();
+}
---
+++ BattleManager.h
@@ -0,0 +1,13 @@
+#ifndef __SFMLS_BATTLE_MANAGER_H__
+#define __SFMLS_BATTLE_MANAGER_H__
+
+#include "Beasts.h"
+
+class BattleManager
+{
+public:
+    static void resolveBattle(Beasts *beasts1, Beasts *beasts2);
+
+};
+
+#endif
