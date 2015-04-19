7f67bfcedb8aebc7d7642681336e11b85c4234ce
Jordi <jordi.nopa@gmail.com>
2015-04-19 20:17:31 +0200

    First approach to upgrades

--- Upgrade.cpp
+++ Upgrade.cpp
@@ -1,11 +1,10 @@
 #include "Upgrade.h"
 
 
-Upgrade::Upgrade(Game *g, sf::Vector2f pos, std::string name, Beast::Attributes bonus) :
-    Object(g, sf::Vector2f(80, 30), Resources::textureSheep, sf::Vector2i(1, 1)),
+Upgrade::Upgrade(Game *g, std::string name, Beast::Attributes bonus, sf::Texture& tex) :
+    Object(g, sf::Vector2f(80, 30), tex, sf::Vector2i(1, 1)),
     name(name), active(false)
 {
-    setPosition(pos);
 }
 
 Upgrade::~Upgrade() {}
